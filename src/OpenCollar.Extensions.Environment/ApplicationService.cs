885f38875a8813c732935dcaacdec5ae3dd7867f
Jonathan Evans <jevans@open-collar.org.uk>
2021-05-01 08:55:47 +0100

    Fixed build script.  Improved comments and docuemntation.

--- src/OpenCollar.Extensions.Environment/ApplicationService.cs
+++ src/OpenCollar.Extensions.Environment/ApplicationService.cs
@@ -1,4 +1,23 @@
-﻿using System.Diagnostics;
+﻿/*
+ * This file is part of OpenCollar.Extensions.Configuration.
+ *
+ * OpenCollar.Extensions.Configuration is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * OpenCollar.Extensions.Configuration is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * OpenCollar.Extensions.Configuration.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright © 2019-2021 Jonathan Evans (jevans@open-collar.org.uk).
+ */
+
+using System.Diagnostics;
 
 using OpenCollar.Extensions.Validation;
 
@@ -91,12 +110,12 @@ namespace OpenCollar.Extensions.Environment
         {
             var isValid = IsValidEnvironmentPairing(resourceName, permitFuzzyResults);
 
-            if (isValid.HasValue && isValid.Value)
+            if(isValid.HasValue && isValid.Value)
             {
                 return;
             }
 
-            if (!isValid.HasValue && permitFuzzyResults)
+            if(!isValid.HasValue && permitFuzzyResults)
             {
                 return;
             }
